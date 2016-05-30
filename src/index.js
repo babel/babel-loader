7f2ec4c03725689f4b7917b757af8aaa7edc4a31
Ognian Tschakalov <ognian.tschakalov@ogi-it.com>
2016-05-30 19:24:06 +0200

    feat(config): add metadataSubscribes option
    - add metadataSubscribers option as an array of context functions to call
    - pass metadata to metadataSubscribers functions
    - add section about cacheDirectory ENOENT error to README.md

--- src/index.js
+++ src/index.js
@@ -56,6 +56,7 @@ const transpile = function(source, options) {
   }
   const code = result.code;
   const map = result.map;
+  const metadata = result.metadata;
 
   if (map && (!map.sourcesContent || !map.sourcesContent.length)) {
     map.sourcesContent = [source];
@@ -64,8 +65,16 @@ const transpile = function(source, options) {
   return {
     code: code,
     map: map,
+    metadata: metadata,
   };
 };
+
+
+function passMetadata(s, context, metadata) {
+  if (context[s]) {
+    context[s](metadata);
+  }
+}
 
 module.exports = function(source, inputSourceMap) {
   let result = {};
@@ -79,6 +88,7 @@ module.exports = function(source, inputSourceMap) {
   const loaderOptions = loaderUtils.parseQuery(this.query);
   const userOptions = assign({}, globalOptions, loaderOptions);
   const defaultOptions = {
+    metadataSubscribers: [],
     inputSourceMap: inputSourceMap,
     sourceRoot: process.cwd(),
     filename: filename,
@@ -107,11 +117,14 @@ module.exports = function(source, inputSourceMap) {
 
   const cacheDirectory = options.cacheDirectory;
   const cacheIdentifier = options.cacheIdentifier;
+  const metadataSubscribers = options.metadataSubscribers;
 
   delete options.cacheDirectory;
   delete options.cacheIdentifier;
+  delete options.metadataSubscribers;
 
   this.cacheable();
+  const context = this;
 
   if (cacheDirectory) {
     const callback = this.async();
@@ -123,10 +136,16 @@ module.exports = function(source, inputSourceMap) {
       transform: transpile,
     }, function(err, result) {
       if (err) { return callback(err); }
+      metadataSubscribers.map(function(s) {
+        passMetadata(s, context, result.metadata);
+      });
       return callback(null, result.code, result.map);
     });
   }
 
   result = transpile(source, options);
+  metadataSubscribers.map(function(s) {
+    passMetadata(s, context, result.metadata);
+  });
   this.callback(null, result.code, result.map);
 };
