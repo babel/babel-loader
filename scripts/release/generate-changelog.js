9438f213d4717df87cf09c4a66fc24e23f50a2dd
Joshua Wiens <joshuaw@easymetrics.com>
2016-08-09 20:16:38 -0500

    style(changelog): fix linting errors

--- scripts/release/generate-changelog.js
+++ scripts/release/generate-changelog.js
@@ -1,10 +1,10 @@
-'use strict'
-const fs = require('fs')
-const addStream = require('add-stream')
-const cl = require('conventional-changelog')
+'use strict';
+const fs = require('fs');
+const addStream = require('add-stream');
+const cl = require('conventional-changelog');
 
-const inStream = fs.createReadStream('CHANGELOG.md')
-const isForce = process.argv.indexOf('--force') !== -1
+const inStream = fs.createReadStream('CHANGELOG.md');
+const isForce = process.argv.indexOf('--force') !== -1;
 
 /**
  * Creates and prepends the changelog from the latest tag to head.
@@ -13,29 +13,27 @@ const isForce = process.argv.indexOf('--force') !== -1
 
 inStream.on('error', function(err) {
   console.error(`Error: failed to read the previous changelog file.
-    If this is the initial run, use the --force flag. ${err}`)
-  process.exit(1)
-})
+    If this is the initial run, use the --force flag. ${err}`);
+  process.exit(1);
+});
 
 const config = {
   preset: 'angular',
   releaseCount: isForce ? 0 : 1
-}
+};
 
 const getOutputStream = () => {
-  return fs.createWriteStream('CHANGELOG.md')
-}
+  return fs.createWriteStream('CHANGELOG.md');
+};
 
-let stream = cl(config)
-  .on('error', function(err) {
-    console.error(`An error occurred while generating the changelog:  ${err}`)
-  })
-  .pipe(!isForce && addStream(inStream) || getOutputStream())
+const stream = cl(config).on('error', function(err) {
+    console.error(`An error occurred while generating the changelog:  ${err}`);
+  }).pipe(!isForce && addStream(inStream) || getOutputStream());
 
 if (!isForce) {
   inStream.on('end', function() {
-    stream.pipe(getOutputStream())
-  })
+    stream.pipe(getOutputStream());
+  });
 }
 
 /*eslint no-console: ["error", { allow: ["warn", "error"] }] */
