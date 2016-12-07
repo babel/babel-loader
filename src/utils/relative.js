80718843c1722eb533b4f372c7271e2c1ac0d536
Daniel Tschinder <daniel.tschinder@researchgate.net>
2016-12-07 17:40:03 +0100

    Always operator on slash and normalize before doing so.

--- src/utils/relative.js
+++ src/utils/relative.js
@@ -1,9 +1,11 @@
 const path = require("path");
 
 module.exports = function relative(sourceRoot, filename) {
-  const rootPath = sourceRoot.split(path.sep)[1];
+  const rootPath = sourceRoot.replace(/\\/g, "/").split("/")[1];
+  const fileRootPath = filename.replace(/\\/g, "/").split("/")[1];
+
   // If the file is in a completely different root folder use the absolute path of file.
-  if (rootPath && rootPath !== filename.split(path.sep)[1]) {
+  if (rootPath && rootPath !== fileRootPath) {
     return filename;
   }
 
