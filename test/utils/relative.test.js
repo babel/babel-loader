cbb769ee9ef8e3f21aa0cc3bb0c69ca0e11dfae7
Daniel Tschinder <daniel.tschinder@researchgate.net>
2016-12-07 17:30:44 +0100

    Make relative util work on windows

--- test/utils/relative.test.js
+++ test/utils/relative.test.js
@@ -1,16 +1,38 @@
 import test from "ava";
+import os from "os";
+import path from "path";
 import relative from "../../lib/utils/relative.js";
 
-test("should get correct relative path", (t) => {
+test("should get correct relative path - depth 0", (t) => {
   t.is(relative("/the/root", "/the/root/one.js"), "one.js");
-  t.is(relative("/the/root", "/the/rootone.js"), "../rootone.js");
-  t.is(relative("/the/root", "/therootone.js"), "/therootone.js");
-
-  t.is(relative("", "/the/root/one.js"), "/the/root/one.js");
-  t.is(relative(".", "/the/root/one.js"), "/the/root/one.js");
-  t.is(relative("", "the/root/one.js"), "the/root/one.js");
-  t.is(relative(".", "the/root/one.js"), "the/root/one.js");
-
-  t.is(relative("/", "/the/root/one.js"), "the/root/one.js");
-  t.is(relative("/", "the/root/one.js"), "the/root/one.js");
 });
+
+test("should get correct relative path - depth 1", (t) => {
+  t.is(relative("/the/root", "/the/rootone.js"), `..${path.sep}rootone.js`);
+});
+
+test("should get correct relative path - depth 2", (t) => {
+  t.is(relative("/the/root", "/therootone.js"), `${path.sep}therootone.js`);
+});
+
+test("should get correct relative path with main root - depth 0", (t) => {
+  t.is(relative("/", "/the/root/one.js"), `the${path.sep}root${path.sep}one.js`);
+});
+
+if (os.platform() === "win32") {
+  test("should get correct relative path - depth 0 - windows", (t) => {
+    t.is(relative("C:\\the\\root", "C:\\the\\root\\one.js"), "one.js");
+  });
+
+  test("should get correct relative path - depth 1 - windows", (t) => {
+    t.is(relative("C:\\the\\root", "C:\\the\\rootone.js"), "..\\rootone.js");
+  });
+
+  test("should get correct relative path - depth 2 - windows", (t) => {
+    t.is(relative("C:\\the\\root", "C:\\therootone.js"), "C:\\therootone.js");
+  });
+
+  test("should get correct relative path with main root - depth 0 - windows", (t) => {
+    t.is(relative("C:\\", "C:\\the\\root\\one.js"), "the\\root\\one.js");
+  });
+}
