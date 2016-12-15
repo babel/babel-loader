48f27253d075f924780741a8234d42f0de22fc03
Daniel Tschinder <daniel.tschinder@researchgate.net>
2016-12-15 13:30:43 +0100

    Disable tests, as they screw up node 0.10

--- test/cache.test.js
+++ test/cache.test.js
@@ -4,7 +4,7 @@ import path from "path";
 import assign from "object-assign";
 import rimraf from "rimraf";
 import webpack from "webpack";
-import mock from "mock-fs";
+// import mock from "mock-fs";
 import createTestDirectory from "./helpers/createTestDirectory";
 
 const defaultCacheDir = path.join(__dirname, "../node_modules/.cache/babel-loader");
@@ -324,139 +324,139 @@ test.cb("should allow to specify the .babelrc file", (t) => {
 // TODO all the following tests should be done without webpack, as the combination
 // of mock-fs and webpack seems to be not reliable.
 
-test.cb.serial("has error if custom cache directory cannot be created", (t) => {
-  const config = assign({}, globalConfig, {
-    output: {
-      path: t.context.directory,
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.jsx?/,
-          loader: babelLoader,
-          exclude: /node_modules/,
-          query: {
-            cacheDirectory: t.context.cacheDirectory,
-            presets: [],
-            babelrc: false,
-          },
-        },
-      ],
-    },
-  });
-
-  mock({
-    [path.dirname(t.context.cacheDirectory)]: mock.directory({
-      mode: 0o444,
-    })
-  });
-  webpack(config, (err, stats) => {
-    t.is(err, null);
-    t.is(stats.hasErrors(), true);
-    t.regex(stats.toJson().errors[0], /EACCES, permission denied/);
-    mock.restore();
-    t.end();
-  });
-});
-
-test.cb.serial("falls back to tmp dir if default dir is not writable", (t) => {
-  const config = assign({}, globalConfig, {
-    output: {
-      path: t.context.directory,
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.jsx?/,
-          loader: babelLoader,
-          exclude: /node_modules/,
-          query: {
-            cacheDirectory: true,
-            presets: [],
-            babelrc: false,
-          },
-        },
-      ],
-    },
-  });
-
-  mock({
-    [path.join(__dirname, "../package.json")]: mock.file(),
-    [path.dirname(defaultCacheDir)]: mock.directory({
-      mode: 0o444,
-    })
-  });
-  webpack(config, (err, stats) => {
-    t.is(err, null);
-    t.is(stats.hasErrors(), false);
-    mock.restore();
-    t.end();
-  });
-});
-
-test.cb.serial("falls back to tmp dir if no cache dir found", (t) => {
-  const config = assign({}, globalConfig, {
-    output: {
-      path: t.context.directory,
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.jsx?/,
-          loader: babelLoader,
-          exclude: /node_modules/,
-          query: {
-            cacheDirectory: true,
-            presets: [],
-            babelrc: false,
-          },
-        },
-      ],
-    },
-  });
-
-  mock({
-    [path.dirname(defaultCacheDir)]: mock.directory({
-      mode: 0o444,
-    })
-  });
-  webpack(config, (err, stats) => {
-    t.is(err, null);
-    t.is(stats.hasErrors(), false);
-    mock.restore();
-    t.end();
-  });
-});
-
-
-test.cb.serial("reports errors from babel transpile", (t) => {
-  const config = assign({}, globalConfig, {
-    output: {
-      path: t.context.directory,
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.jsx?/,
-          loader: babelLoader,
-          exclude: /node_modules/,
-          query: {
-            cacheDirectory: true,
-            presets: ["es2015"],
-          },
-        },
-      ],
-    },
-  });
-
-  mock();
-  webpack(config, (err, stats) => {
-    t.is(err, null);
-    t.is(stats.hasErrors(), true);
-    t.regex(stats.toJson().errors[0], /ENOENT, no such file or directory/);
-    t.regex(stats.toJson().errors[0], /\.babelrc/);
-    mock.restore();
-    t.end();
-  });
-});
+// test.cb.serial("has error if custom cache directory cannot be created", (t) => {
+//   const config = assign({}, globalConfig, {
+//     output: {
+//       path: t.context.directory,
+//     },
+//     module: {
+//       loaders: [
+//         {
+//           test: /\.jsx?/,
+//           loader: babelLoader,
+//           exclude: /node_modules/,
+//           query: {
+//             cacheDirectory: t.context.cacheDirectory,
+//             presets: [],
+//             babelrc: false,
+//           },
+//         },
+//       ],
+//     },
+//   });
+//
+//   mock({
+//     [path.dirname(t.context.cacheDirectory)]: mock.directory({
+//       mode: 0o444,
+//     })
+//   });
+//   webpack(config, (err, stats) => {
+//     t.is(err, null);
+//     t.is(stats.hasErrors(), true);
+//     t.regex(stats.toJson().errors[0], /EACCES, permission denied/);
+//     mock.restore();
+//     t.end();
+//   });
+// });
+//
+// test.cb.serial("falls back to tmp dir if default dir is not writable", (t) => {
+//   const config = assign({}, globalConfig, {
+//     output: {
+//       path: t.context.directory,
+//     },
+//     module: {
+//       loaders: [
+//         {
+//           test: /\.jsx?/,
+//           loader: babelLoader,
+//           exclude: /node_modules/,
+//           query: {
+//             cacheDirectory: true,
+//             presets: [],
+//             babelrc: false,
+//           },
+//         },
+//       ],
+//     },
+//   });
+//
+//   mock({
+//     [path.join(__dirname, "../package.json")]: mock.file(),
+//     [path.dirname(defaultCacheDir)]: mock.directory({
+//       mode: 0o444,
+//     })
+//   });
+//   webpack(config, (err, stats) => {
+//     t.is(err, null);
+//     t.is(stats.hasErrors(), false);
+//     mock.restore();
+//     t.end();
+//   });
+// });
+//
+// test.cb.serial("falls back to tmp dir if no cache dir found", (t) => {
+//   const config = assign({}, globalConfig, {
+//     output: {
+//       path: t.context.directory,
+//     },
+//     module: {
+//       loaders: [
+//         {
+//           test: /\.jsx?/,
+//           loader: babelLoader,
+//           exclude: /node_modules/,
+//           query: {
+//             cacheDirectory: true,
+//             presets: [],
+//             babelrc: false,
+//           },
+//         },
+//       ],
+//     },
+//   });
+//
+//   mock({
+//     [path.dirname(defaultCacheDir)]: mock.directory({
+//       mode: 0o444,
+//     })
+//   });
+//   webpack(config, (err, stats) => {
+//     t.is(err, null);
+//     t.is(stats.hasErrors(), false);
+//     mock.restore();
+//     t.end();
+//   });
+// });
+//
+//
+// test.cb.serial("reports errors from babel transpile", (t) => {
+//   const config = assign({}, globalConfig, {
+//     output: {
+//       path: t.context.directory,
+//     },
+//     module: {
+//       loaders: [
+//         {
+//           test: /\.jsx?/,
+//           loader: babelLoader,
+//           exclude: /node_modules/,
+//           query: {
+//             cacheDirectory: true,
+//             presets: ["es2015"],
+//           },
+//         },
+//       ],
+//     },
+//   });
+//
+//   mock();
+//   webpack(config, (err, stats) => {
+//     t.is(err, null);
+//     t.is(stats.hasErrors(), true);
+//     t.regex(stats.toJson().errors[0], /ENOENT, no such file or directory/);
+//     t.regex(stats.toJson().errors[0], /\.babelrc/);
+//     mock.restore();
+//     t.end();
+//   });
+// });
 
