d6ed649c4b0d4f062400066d2d1b4a8bfba330ed
Edi Duluman <edi@duluman.ro>
2017-02-28 17:02:15 +0200

    fix deprecation warning
    Fixes deprecation warning for webpack 2.x+ while also keeps on working with older webpack versions.
    
    DeprecationWarning: loaderUtils.parseQuery() received a non-string value which can be problematic, see (https://github.com/webpack/loader-utils/issues/56)[https://github.com/webpack/loader-utils/issues/56]
    
    parseQuery() will be replaced with getOptions() in the next major version of loader-utils.

--- src/index.js
+++ src/index.js
@@ -87,7 +87,7 @@ module.exports = function(source, inputSourceMap) {
 
   // Handle options
   const globalOptions = this.options.babel || {};
-  const loaderOptions = loaderUtils.parseQuery(this.query);
+  const loaderOptions = loaderUtils.getOptions ? loaderUtils.getOptions(this) || {} : loaderUtils.parseQuery(this.query);
   const userOptions = assign({}, globalOptions, loaderOptions);
   const defaultOptions = {
     inputSourceMap: inputSourceMap,
