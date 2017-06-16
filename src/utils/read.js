8544ffad99ea877d984d505e7bd7ff2f5d762895
Daniel Tschinder <daniel@tschinder.de>
2017-06-16 18:33:29 +0200

    Only base cache identifier on babel options from pkg.json (#468)

--- src/utils/read.js
+++ src/utils/read.js
@@ -1,6 +1,10 @@
-module.exports = function(fileSystem, filename) {
-  if (!filename) {
-    throw new Error("filename must be a string");
+const path = require("path");
+
+module.exports = function readBabelConfig(fileSystem, filename) {
+  if (path.basename(filename) === "package.json") {
+    const pkg = require(filename);
+
+    return JSON.stringify(pkg.babel);
   }
 
   // Webpack `fs` return Buffer
--- test/utils/read.test.js
+++ test/utils/read.test.js
@@ -5,6 +5,7 @@ import read from "../../lib/utils/read.js";
 
 const files = {
   existent: path.join(__dirname, "../fixtures/basic.js"),
+  pkg: path.join(__dirname, "../fixtures/package-test/package.json"),
 };
 
 const content = fs.readFileSync(files.existent, "utf8");
@@ -12,4 +13,9 @@ const content = fs.readFileSync(files.existent, "utf8");
 test("should return contents if file exists", t => {
   const realFile = read(fs, files.existent);
   t.is(realFile, content);
+});
+
+test("should only return config from package.json", t => {
+  const realFile = read(fs, files.pkg);
+  t.is(realFile, '{"stage":3}');
 });
