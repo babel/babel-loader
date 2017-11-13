27f44f8a7bbf0079673ae50bb31cb84e22b764ae
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2017-11-13 07:23:38 +0100

    style(src): modernize code

--- src/utils/relative.js
+++ src/utils/relative.js
@@ -1,13 +1,14 @@
 const path = require("path");
 
-module.exports = function relative(sourceRoot, filename) {
-  const rootPath = sourceRoot.replace(/\\/g, "/").split("/")[1];
-  const fileRootPath = filename.replace(/\\/g, "/").split("/")[1];
+module.exports = function relative(root, file) {
+  const rootPath = root.replace(/\\/g, "/").split("/")[1];
+  const filePath = file.replace(/\\/g, "/").split("/")[1];
 
-  // If the file is in a completely different root folder use the absolute path of file.
-  if (rootPath && rootPath !== fileRootPath) {
-    return filename;
+  // If the file is in a completely different root folder
+  // use the absolute path of the file
+  if (rootPath && rootPath !== filePath) {
+    return file;
   }
 
-  return path.relative(sourceRoot, filename);
+  return path.relative(root, file);
 };
