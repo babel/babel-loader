a2d86d9a1ed8a5b4f33b503e07af6935f119c7a2
Izaak Schroeder <izaak.schroeder@gmail.com>
2018-06-21 19:42:53 -0700

    Return more data from `babel.transform`. (#629)
    Previous code only returns an object of the shape `{ code, map, metadata }` which prevents accessing things like the AST when `babel` is configured to output it. This change allows access to more things that `babel` spits out.

--- src/transform.js
+++ src/transform.js
@@ -14,13 +14,18 @@ module.exports = async function(source, options) {
 
   if (!result) return null;
 
-  const { code, map, metadata } = result;
+  // We don't return the full result here because some entries are not
+  // really serializable. For a full list of properties see here:
+  // https://github.com/babel/babel/blob/master/packages/babel-core/src/transformation/index.js
+  // For discussion on this topic see here:
+  // https://github.com/babel/babel-loader/pull/629
+  const { ast, code, map, metadata, sourceType } = result;
 
   if (map && (!map.sourcesContent || !map.sourcesContent.length)) {
     map.sourcesContent = [source];
   }
 
-  return { code, map, metadata };
+  return { ast, code, map, metadata, sourceType };
 };
 
 module.exports.version = babel.version;
