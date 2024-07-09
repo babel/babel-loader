30a626076fa71089ac60f2fe2bc1d824170e5278
Huáng Jùnliàng <jlhwung@gmail.com>
2024-07-09 15:19:08 -0400

    rewrite metadata test

--- test/metadata.test.js
+++ test/metadata.test.js
@@ -1,37 +1,42 @@
 import test from "ava";
-import fs from "fs";
 import path from "path";
 import { rimraf } from "rimraf";
 import createTestDirectory from "./helpers/createTestDirectory.js";
 import { webpackAsync } from "./helpers/webpackAsync.js";
-import ReactIntlPlugin from "react-intl-webpack-plugin";
+import { NormalModule } from "webpack";
 
 const cacheDir = path.join(__dirname, "output/cache/cachefiles");
 const outputDir = path.join(__dirname, "output/metadata");
 const babelLoader = path.join(__dirname, "../lib");
-const globalConfig = {
-  mode: "development",
-  entry: "./test/fixtures/metadata.js",
-  output: {
-    path: outputDir,
-    filename: "[id].metadata.js",
-  },
-  plugins: [new ReactIntlPlugin()],
-  module: {
-    rules: [
-      {
-        test: /\.jsx?/,
-        loader: babelLoader,
-        options: {
-          metadataSubscribers: [ReactIntlPlugin.metadataContextFunctionName],
-          plugins: ["react-intl"],
-          presets: [],
-        },
-        exclude: /node_modules/,
+
+function babelMetadataProvierPlugin() {
+  return {
+    name: "babel-metadata-provider-plugin",
+    visitor: {
+      Program(_, pass) {
+        pass.file.metadata = { hello: "world" };
       },
-    ],
-  },
-};
+    },
+  };
+}
+
+class WebpackMetadataSubscriberPlugin {
+  static subscriber = Symbol("subscriber");
+  constructor(subscriberCallback) {
+    this.subscriberCallback = subscriberCallback;
+  }
+  apply(compiler) {
+    compiler.hooks.compilation.tap("plugin", compilation => {
+      NormalModule.getCompilationHooks(compilation).loader.tap(
+        "plugin",
+        context => {
+          context[WebpackMetadataSubscriberPlugin.subscriber] =
+            this.subscriberCallback;
+        },
+      );
+    });
+  }
+}
 
 // Create a separate directory for each test so that the tests
 // can run in parallel
@@ -42,94 +47,81 @@ test.beforeEach(async t => {
 
 test.afterEach(t => rimraf(t.context.directory));
 
-test("should pass metadata code snippet", async t => {
-  const config = Object.assign({}, globalConfig, {
+test("should obtain metadata from the transform result", async t => {
+  let actualMetadata;
+
+  const config = {
+    mode: "development",
+    entry: "./test/fixtures/basic.js",
     output: {
       path: t.context.directory,
       filename: "[id].metadata.js",
     },
-  });
-
-  const stats = await webpackAsync(config);
-  t.deepEqual(stats.compilation.errors, []);
-  t.deepEqual(stats.compilation.warnings, []);
-
-  const files = fs.readdirSync(t.context.directory);
-  t.true(files.length > 0);
-
-  const text = fs.readFileSync(
-    path.resolve(t.context.directory, "reactIntlMessages.json"),
-    "utf8",
-  );
-  const jsonText = JSON.parse(text);
-  t.true(jsonText.length == 1);
-  t.true(jsonText[0].id == "greetingId");
-  t.true(jsonText[0].defaultMessage == "Hello World!");
-});
-
-test("should not throw error", async t => {
-  const config = Object.assign({}, globalConfig, {
-    output: {
-      path: t.context.directory,
-      filename: "[id].metadata.js",
-    },
-  });
-
-  const stats = await webpackAsync(config);
-  t.deepEqual(stats.compilation.errors, []);
-  t.deepEqual(stats.compilation.warnings, []);
-});
-
-test("should throw error", async t => {
-  const config = Object.assign({}, globalConfig, {
-    output: {
-      path: t.context.directory,
-      filename: "[id].metadata.js",
-    },
-    entry: "./test/fixtures/metadataErr.js",
-  });
-
-  const stats = await webpackAsync(config);
-  t.true(stats.compilation.errors.length > 0);
-  t.deepEqual(stats.compilation.warnings, []);
-});
-
-test("should pass metadata code snippet ( cache version )", async t => {
-  const config = Object.assign({}, globalConfig, {
-    output: {
-      path: t.context.directory,
-      filename: "[id].metadata.js",
-    },
+    plugins: [
+      new WebpackMetadataSubscriberPlugin(
+        metadata => (actualMetadata = metadata),
+      ),
+    ],
     module: {
       rules: [
         {
-          test: /\.jsx?/,
+          test: /\.js/,
           loader: babelLoader,
           options: {
-            metadataSubscribers: [ReactIntlPlugin.metadataContextFunctionName],
-            plugins: ["react-intl"],
-            cacheDirectory: cacheDir,
-            presets: [],
+            metadataSubscribers: [WebpackMetadataSubscriberPlugin.subscriber],
+            plugins: [babelMetadataProvierPlugin],
+            babelrc: false,
+            configFile: false,
           },
           exclude: /node_modules/,
         },
       ],
     },
-  });
+  };
 
   const stats = await webpackAsync(config);
   t.deepEqual(stats.compilation.errors, []);
   t.deepEqual(stats.compilation.warnings, []);
 
-  const files = fs.readdirSync(t.context.directory);
-  t.true(files.length > 0);
+  t.deepEqual(actualMetadata, { hello: "world" });
+});
 
-  const text = fs.readFileSync(
-    path.resolve(t.context.directory, "reactIntlMessages.json"),
-    "utf8",
-  );
-  const jsonText = JSON.parse(text);
-  t.true(jsonText.length == 1);
-  t.true(jsonText[0].id == "greetingId");
-  t.true(jsonText[0].defaultMessage == "Hello World!");
+test("should obtain metadata from the transform result with cache", async t => {
+  let actualMetadata;
+
+  const config = {
+    mode: "development",
+    entry: "./test/fixtures/basic.js",
+    output: {
+      path: t.context.directory,
+      filename: "[id].metadata.js",
+    },
+    plugins: [
+      new WebpackMetadataSubscriberPlugin(
+        metadata => (actualMetadata = metadata),
+      ),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js/,
+          loader: babelLoader,
+          options: {
+            cacheDirectory: cacheDir,
+            metadataSubscribers: [WebpackMetadataSubscriberPlugin.subscriber],
+            plugins: [babelMetadataProvierPlugin],
+            babelrc: false,
+            configFile: false,
+          },
+          exclude: /node_modules/,
+        },
+      ],
+    },
+  };
+
+  const stats = await webpackAsync(config);
+  t.deepEqual(stats.compilation.errors, []);
+  t.deepEqual(stats.compilation.warnings, []);
+
+  t.deepEqual(actualMetadata, { hello: "world" });
 });
